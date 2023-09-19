f414e4e44a099217b51b2be3bac79ed84adfe796
Mohammed Subhan <mohdsubhan2764@gmail.com>
2023-09-19 22:51:29 +0530

    user login and logout in django

--- accounts/views.py
+++ accounts/views.py
@@ -23,10 +23,29 @@ def register(request):
                 user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
                 user.save()
                 print("user created")
+                return redirect('login')
                 
         else:
             messages.info(request,"Password doesn't match")
             return redirect('register')
-        return redirect('/')
     else:
         return render(request,'register.html')
+
+
+def login(request):
+    if request.method =='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
